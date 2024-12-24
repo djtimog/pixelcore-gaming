d1f4cfd627146eff54626930f19ebe64f4d116b0
djtimog <ogunleyetimilehin15@gmail.com>
2024-12-24 19:34:33 +0000

    feat: add background image and integrate Sass for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
+import styles from "./app.module.scss"
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
+        <section>
+          <div className={`h-[92vh] ${styles.bgImage}`}></div>
+        </section>
       </main>
       <Footer />
     </>
