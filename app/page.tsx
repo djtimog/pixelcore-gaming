748f837ccc43913df306c61ff9ad0eb721f7799d
djtimog <ogunleyetimilehin15@gmail.com>
2024-12-24 17:43:37 +0000

    starting all over

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
-import Hero from "@/ui/hero-section";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <Hero />
       </main>
       <Footer />
     </>
