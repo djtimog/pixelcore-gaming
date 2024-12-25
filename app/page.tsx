2dbd32ec84c740abb7c55324279e5236f4afc7ca
djtimog <ogunleyetimilehin15@gmail.com>
2024-12-25 15:12:36 +0000

    feat: add background video support and enhance hero section styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
+'use client'
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
 import styles from "./app.module.scss"
+import { useTheme } from "@/app/context/theme-context";
 
 export default function Home() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <>
       <Header />
       <main>
         <section>
-          <div className={`h-[92vh] ${styles.bgImage}`}>
-            <div className="flex justify-center items-center px-5">
-              <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold text-center"> Pixel <span className="text-[#14C570]">C</span>ore Esport <br/>Team</h1>
-                {/* <div class="text-container">
-              <span style="--i: 0">Cricket</span>
-              <span style="--i: 1">Football</span>
-              <span style="--i: 2">Hockey</span>
-              <span style="--i: 3">Food</span>
-              <span style="--i: 4">Travel</span>
-          </div> */}
+          <div className={`${styles.hero}`}>
+            <video 
+              src={theme === "light" ? "/white-bg-video.mp4"  : "/black-bg-video.mp4"} 
+              autoPlay 
+              muted 
+              loop 
+            />
+            <div className="flex justify-center items-center px-5 py-5">
+              <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-bold text-center"> Pixel <span className="text-[#14C570]">C</span>ore Esport <br/>Team</h1>
             </div>
           </div>
         </section>
