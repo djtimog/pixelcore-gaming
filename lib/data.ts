9e06766988b1eb4da566a91460b5df57363e8f04
djtimog <ogunleyetimilehin15@gmail.com>
2025-02-03 00:40:15 +0000

    feat: add game data structure and remove obsolete type definitions

--- lib/data.ts
+++ lib/data.ts
@@ -122,25 +122,103 @@ export const teams = [
           rank: "Gold",
           level: 250,
         },
-        {
-          name: "Bob Brown",
-          ign: "tcr-king.77",
-          image: "/captain.png",
-          role: "Player",
-          igRole: "Striker",
-          rank: "Silver",
-          level: 200,
-        },
-        {
-          name: "Alice Johnson",
-          ign: "tcr-queen.99",
-          image: "/captain.png",
-          role: "Player",
-          igRole: "Support",
-          rank: "Gold",
-          level: 250,
-        },
       ],
     },
   ];
+  
+  const games = [
+    {
+      name: "Call of Duty",
+      genre: "First-Person Shooter",
+      platform: "PC, PlayStation, Xbox",
+      publisher: "Activision"
+    },
+    {
+      name: "League of Legends",
+      genre: "MOBA",
+      platform: "PC",
+      publisher: "Riot Games"
+    },
+    {
+      name: "Minecraft",
+      genre: "Sandbox",
+      platform: "PC, Mobile, Console",
+      publisher: "Mojang Studios"
+    },
+    {
+      name: "Fortnite",
+      genre: "Battle Royale",
+      platform: "PC, PlayStation, Xbox, Mobile",
+      publisher: "Epic Games"
+    },
+    {
+      name: "Valorant",
+      genre: "First-Person Shooter",
+      platform: "PC",
+      publisher: "Riot Games"
+    },
+    {
+      name: "Apex Legends",
+      genre: "Battle Royale",
+      platform: "PC, PlayStation, Xbox, Nintendo Switch",
+      publisher: "Electronic Arts"
+    },
+    {
+      name: "Overwatch 2",
+      genre: "First-Person Shooter",
+      platform: "PC, PlayStation, Xbox, Nintendo Switch",
+      publisher: "Blizzard Entertainment"
+    },
+    {
+      name: "Dota 2",
+      genre: "MOBA",
+      platform: "PC",
+      publisher: "Valve"
+    },
+    {
+      name: "Rocket League",
+      genre: "Sports",
+      platform: "PC, PlayStation, Xbox, Nintendo Switch",
+      publisher: "Psyonix"
+    },
+    {
+      name: "Counter-Strike 2",
+      genre: "First-Person Shooter",
+      platform: "PC",
+      publisher: "Valve"
+    },
+    {
+      name: "PUBG: Battlegrounds",
+      genre: "Battle Royale",
+      platform: "PC, PlayStation, Xbox, Mobile",
+      publisher: "Krafton"
+    },
+    {
+      name: "Rainbow Six Siege",
+      genre: "Tactical Shooter",
+      platform: "PC, PlayStation, Xbox",
+      publisher: "Ubisoft"
+    },
+    {
+      name: "The Legend of Zelda: Breath of the Wild",
+      genre: "Action-Adventure",
+      platform: "Nintendo Switch",
+      publisher: "Nintendo"
+    },
+    {
+      name: "Super Smash Bros. Ultimate",
+      genre: "Fighting",
+      platform: "Nintendo Switch",
+      publisher: "Nintendo"
+    },
+    {
+      name: "FIFA 23",
+      genre: "Sports",
+      platform: "PC, PlayStation, Xbox, Nintendo Switch",
+      publisher: "Electronic Arts"
+    }
+  ];
+  
+
+  
   
